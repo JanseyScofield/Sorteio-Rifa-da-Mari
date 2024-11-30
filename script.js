2636330e03ca3cccc7f23c410afa95dcf77d297e
Jansey Scofield <janscofield10@gmail.com>
2024-11-30 02:17:33 -0300

    Adicionando animação de girar a roleta

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let iCont;
 const qtdNumeros = 120;
 const numerosCadastrados = document.getElementById('bloco-numeros');
 const numeroSorteado = document.getElementById('numero-sorteado');
+const roleta = document.getElementById('roleta');
 
 function preencherTabelaRifa(){
     for(iCont = 1; iCont <= qtdNumeros; iCont++){
@@ -17,7 +18,16 @@ function preencherTabelaRifa(){
 }
 
 function sortearNumero(){
-    numeroSorteado.textContent = Math.floor(Math.random() * qtdNumeros) + 1;
+    numeroSorteado.textContent = '-';
+    roleta.classList.add('animacao-sorteio');
+    setTimeout(() => {
+        numeroSorteado.textContent = Math.floor(Math.random() * qtdNumeros) + 1;
+        roleta.classList.remove('animacao-sorteio');
+        roleta.classList.add('fim-sorteio');
+        setTimeout(() => {
+            roleta.classList.remove('fim-sorteio');
+        }, 1000);
+    }, 5000);
 }
 
 preencherTabelaRifa();
